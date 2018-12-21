42e6b7d517188aa12f3bdb463fa0abc440f1f060
Axel Poty <axel.poty@free.fr>
2018-12-21 16:24:54 +0100

    id

--- src/models/index.js
+++ src/models/index.js
@@ -25,7 +25,16 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.sequelize
+    .sync({ force: true })
+    .then(function(err) {
+        console.log('It worked!');
+    }, function (err) {
+        console.log('An error occurred while creating the table:', err);
+    });
 
 module.exports = db;
